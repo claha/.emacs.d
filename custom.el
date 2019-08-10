cc3cf5317b264fc6781e07111dc0275072f07a88
Claes Hallström <hallstrom.claes@gmail.com>
2019-08-10 18:41:01 +0200

    Enable hungry-delete, remove all whitespace

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (avy ivy which-key htmlize org-bullets material-theme auto-compile use-package))))
+    (hungry-delete avy ivy which-key htmlize org-bullets material-theme auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
