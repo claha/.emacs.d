31eb18c17acd1460d6e8350698eab933a22c6dc2
Claes Hallström <hallstrom.claes@gmail.com>
2019-08-10 09:34:57 +0200

    Setup MELPA and use-package in init.el

---
+++ init.el
@@ -0,0 +1,9 @@
+;; Configure package.el to include MELPA.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
