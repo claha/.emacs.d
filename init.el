1ae1c6284acf2aab124a43a07eba793e81456a78
Claes Hallstrom <claes.hallstrom@actia.se>
2019-09-06 09:35:06 +0200

    Configure garbage collection at startup

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; Make startup faster by reducing the frequency of garbage collection.
+(setq gc-cons-threshold (* 50 1000 1000))
+
 ;; Configure package.el to include MELPA.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -10,3 +13,6 @@
 
 ;; Load configuration
 (org-babel-load-file "~/.emacs.d/conf.org")
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
