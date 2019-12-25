e637504ff44403608641aede44afa8057c9f8270
Claes Hallström <hallstrom.claes@gmail.com>
2019-12-25 09:01:49 +0100

    Add ace-window

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy-rich diff-hl magit counsel rg dumb-jump ag yasnippet go-mode hydra cmake-mode flycheck git-commit doom-modeline hungry-delete avy ivy which-key htmlize org-bullets material-theme auto-compile use-package))))
+    (ace-window ivy-rich diff-hl magit counsel rg dumb-jump ag yasnippet go-mode hydra cmake-mode flycheck git-commit doom-modeline hungry-delete avy ivy which-key htmlize org-bullets material-theme auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
